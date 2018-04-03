47a24cb59708679c3226d7b6a443d1e6b7b64fcd
Nicolai <nicolai@rosenvinge.dk>
2018-04-03 12:58:32 +0200

    fixed wait timeout

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -1,14 +1,14 @@
 package DBAccess;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
-//    private static final String url = "jdbc:mysql://localhost:3306/useradmin?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-//    private static final String username = "doorkeeper";
-//    private static final String password = "bank3*andyouarein";
-//
+    private static final String url = "jdbc:mysql://165.227.169.82:3306/useradmin?autoReconnect=true";
+    private static final String username = "sunicolai";
+    private static final String password = "root";
+
     private static Connection singleton;
 //
     public static void setConnection(Connection con) {
@@ -18,27 +18,27 @@ public class Connector {
 
 
     
-//
-//    public static Connection connection() throws ClassNotFoundException, SQLException  {
-//        if ( singleton == null ) {
-//            Class.forName( "com.mysql.jdbc.Driver" );
-//            singleton = DriverManager.getConnection( url, username, password );
-//        }
-//        return singleton;
-//    }
-//}
 
-    public static Connection connection() throws ClassNotFoundException, SQLException {
-        if (singleton == null) {
-            MysqlDataSource source = new MysqlDataSource();
-            source.setServerName("165.227.169.82");
-            source.setPort(3306);
-            source.setDatabaseName("useradmin");
-            source.setUser("sunicolai");
-            source.setPassword("root");
-            singleton = source.getConnection();
+    public static Connection connection() throws ClassNotFoundException, SQLException  {
+        if ( singleton == null ) {
+            Class.forName( "com.mysql.jdbc.Driver" );
+            singleton = DriverManager.getConnection( url, username, password );
         }
         return singleton;
     }
 }
+
+//    public static Connection connection() throws ClassNotFoundException, SQLException {
+//        if (singleton == null) {
+//            MysqlDataSource source = new MysqlDataSource();
+//            source.setServerName("165.227.169.82");
+//            source.setPort(3306);
+//            source.setDatabaseName("useradmin");
+//            source.setUser("sunicolai");
+//            source.setPassword("root");
+//            singleton = source.getConnection();
+//        }
+//        return singleton;
+//    }
+//}
 
